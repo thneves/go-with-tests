fd4ad3bfcdf9d57525c85c18b2c8f53142dae2ab
thneves <thales.neves@pinpeople.com.br>
2025-04-13 21:19:52 -0300

    more tests more slices

--- sum.go
+++ sum.go
@@ -1,7 +1,6 @@
 package main
 
 func Sum(numbers []int) int {
-
 	sum := 0
 
 	for _, number := range numbers {
@@ -9,4 +8,28 @@ func Sum(numbers []int) int {
 	}
 
 	return sum
+}
+
+func SumAll(numbersToSum ...[]int) []int {
+	sizeOfSlicesToSum := len(numbersToSum)
+
+	sums := make([]int, sizeOfSlicesToSum)
+
+	for _, numbers := range numbersToSum {
+		newSum := Sum(numbers)
+		sums = append(sums, newSum)
+	}
+
+	return sums
+}
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+
+	for _, numbers := range numbersToSum {
+		tail := numbers[1:]
+		sums = append(sums, Sum(tail))
+	}
+
+	return sums
 }
