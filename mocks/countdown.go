a8ceea7535268ee810468919a704cb06f998178e
thneves <thales.neves@pinpeople.com.br>
2025-05-02 18:57:08 -0300

    mocks draft

---
+++ mocks/countdown.go
@@ -0,0 +1,23 @@
+package mocks
+
+import (
+	"fmt"
+	"io"
+	"time"
+)
+
+const finalWord = "Go!"
+const countdownStart = 3
+
+type Sleeper interface {
+	Sleep()
+}
+
+func Countdown(out io.Writer, sleeper Sleeper) {
+	for i := countdownStart; i > 0; i-- {
+		fmt.Fprintln(out, i)
+		time.Sleep(1 * time.Second)
+	}
+
+	fmt.Fprint(out, finalWord)
+}
---
+++ mocks/countdown_test.go
@@ -0,0 +1,35 @@
+package mocks
+
+import (
+	"bytes"
+	"testing"
+)
+
+type SpySleeper struct {
+	Calls int
+}
+
+func (s *SpySleeper) Sleep() {
+	s.Calls++
+}
+
+func TestCountdown(t *testing.T) {
+	buffer := &bytes.Buffer{}
+	spySleeper := &SpySleeper{}
+
+	Countdown(buffer, spySleeper)
+
+	got := buffer.String()
+	want := `3
+2
+1
+Go!`
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+
+	if spySleeper.Calls != 3 {
+		t.Errorf("not enought calls to sleep, want 3 got %d", spySleeper.Calls)
+	}
+}
